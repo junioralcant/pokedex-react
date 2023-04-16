ff6740e888b01d791309491d0098a1e797a6bc01
junioralcant <junioralcant111@gmail.com>
2023-04-16 17:47:35 -0300

    test: ensure with Home return pokemons list

---
+++ src/pages/home/home.spec.tsx
@@ -0,0 +1,26 @@
+import {render, screen, waitFor} from '@testing-library/react';
+import {Home} from '.';
+import {list} from '../../service/load-pokemon-list/remote-load-pokemon-list';
+import {mockLoadPokemonList} from '../../service/load-pokemon-list/mocks/pokemon';
+
+function makeSut() {
+  render(<Home />);
+}
+
+jest.mock(
+  '../../service/load-pokemon-list/remote-load-pokemon-list',
+  () => ({
+    list: jest.fn(() => mockLoadPokemonList()),
+  })
+);
+describe('Home', () => {
+  it('Should return list pokemon', async () => {
+    makeSut();
+
+    const pokemonsContent = screen.getByTestId('box-pokemon');
+    await waitFor(() => pokemonsContent);
+
+    expect(list).toHaveBeenCalled();
+    expect(pokemonsContent.children.length).toBe(2);
+  });
+});
