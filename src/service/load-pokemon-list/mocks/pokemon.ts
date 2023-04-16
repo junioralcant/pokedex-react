a01a7c1e4b018b0658508dd0aee378ea40c7fee6
junioralcant <junioralcant111@gmail.com>
2023-04-16 15:57:37 -0300

    test: ensure with RemoteLoadPokemon return correct a pokemon list

---
+++ src/service/load-pokemon-list/mocks/pokemon.ts
@@ -0,0 +1,24 @@
+export function mockLoadPokemonList(): ILoadPokemonList.Model[] {
+  return [
+    {
+      id: '123',
+      img: 'url_img',
+      name: 'pkemon-name',
+      skills: [
+        {
+          name: 'habilidade',
+        },
+      ],
+    },
+    {
+      id: '13',
+      img: 'url_img',
+      name: 'pkemon-name',
+      skills: [
+        {
+          name: 'habilidade',
+        },
+      ],
+    },
+  ];
+}
---
+++ src/service/load-pokemon-list/remote-load-pokemon-list.spec.ts
@@ -0,0 +1,23 @@
+import {list} from './remote-load-pokemon-list';
+import {mockLoadPokemonList} from './mocks/pokemon';
+
+type SutType = {
+  list: () => Promise<ILoadPokemonList.Model[]>;
+};
+
+function makeSut(): SutType {
+  return {
+    list,
+  };
+}
+
+describe('RemoteLoadPokemonList', () => {
+  it('should return a pokemon list ', async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(sut, 'list')
+      .mockResolvedValueOnce(mockLoadPokemonList());
+    const response = await sut.list();
+    expect(response).toEqual(mockLoadPokemonList());
+  });
+});
